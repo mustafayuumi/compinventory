7d7ad4121530695bc57534f90da7967bce26a9d7
Mustafa Kırmızıoğlu <kirmizimustafa987@gmail.com>
2026-09-19 16:22:40 +0300

    exposed dependency added

---
+++ server/compinv_server/build.gradle.kts
@@ -0,0 +1,30 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+}
+
+group = "com.mustafayuumi"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+
+    //exposed
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.h2)
+}
---
+++ server/compinv_server/src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+package com.mustafayuumi
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
